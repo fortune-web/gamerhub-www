b10aa0ef3140ae490b667db3a7b674b9f840cfa2
fortune-web <daniel.wong9503@gmail.com>
2022-02-04 21:42:26 +0100

    Initial dev prep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta name="description" content="Multi-chain games aggregator" />
+      <title>GamerHub</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
-export default MyApp
+export default MyApp;
