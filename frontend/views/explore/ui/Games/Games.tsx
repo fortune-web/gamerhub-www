9467db23662199270f6e88d698a22dd919a96485
fortune-web <daniel.wong9503@gmail.com>
2022-05-12 23:30:29 +0200

    feat: add three provided game images and links

--- frontend/views/explore/ui/Games/Games.tsx
+++ frontend/views/explore/ui/Games/Games.tsx
@@ -3,6 +3,9 @@ import mockImage from './assets/img/assets-1.jpg';
 import { AllGenresIcon, NewReleaseIcon } from '../Icons/Icons';
 import styles from './Games.module.scss';
 import classNames from 'classnames';
+import Abeat from './assets/img/Abeats.jpg';
+import MetaMoba from './assets/img/MetaMoba.jpg';
+import Warhands from './assets/img/Warhands.jpg';
 
 export const Games = () => {
   return (
@@ -22,13 +25,43 @@ export const Games = () => {
               <div
                 className={classNames('part-img', styles.gameImageContainer)}
               >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
+                <Image src={Warhands} alt="Game" width={500} height={500} />
               </div>
               <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
+                <h4 className="game-title">WarHands NFT</h4>
+                <a href="https://bit.ly/3yj72HS" className="def-btn def-small">
                   Play Now
                 </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-sm-6">
+            <div className="single-game">
+              <div
+                className={classNames('part-img', styles.gameImageContainer)}
+              >
+                <Image src={MetaMoba} alt="Game" width={500} height={500} />
+              </div>
+              <div className="part-text">
+                <h4 className="game-title">Meta Moba</h4>
+                <a
+                  href="https://play.google.com/apps/test/RQPfvpaiK-o/ahAIGJVrfT42zmnBb2u8J7oRp3Mt1kbWb-INihTvYEQynogGKVNRy23UIt5AgPhCSwtjFWxW_fJF3HpVmtebgEpUau"
+                  className="def-btn def-small"
+                >
+                  Play Now
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-sm-6">
+            <div className="single-game">
+              <div
+                className={classNames('part-img', styles.gameImageContainer)}
+              >
+                <Image src={Abeat} alt="Game" width={500} height={500} />
+              </div>
+              <div className="part-text">
+                <h4 className="game-title">Abeats Games</h4>
               </div>
             </div>
           </div>
@@ -40,10 +73,7 @@ export const Games = () => {
                 <Image src={mockImage} alt="Game" width={500} height={500} />
               </div>
               <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
+                <h4 className="game-title">Coming Soon</h4>
               </div>
             </div>
           </div>
@@ -55,10 +85,7 @@ export const Games = () => {
                 <Image src={mockImage} alt="Game" width={500} height={500} />
               </div>
               <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
+                <h4 className="game-title">Coming Soon</h4>
               </div>
             </div>
           </div>
@@ -70,10 +97,7 @@ export const Games = () => {
                 <Image src={mockImage} alt="Game" width={500} height={500} />
               </div>
               <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
+                <h4 className="game-title">Coming Soon</h4>
               </div>
             </div>
           </div>
@@ -85,10 +109,7 @@ export const Games = () => {
                 <Image src={mockImage} alt="Game" width={500} height={500} />
               </div>
               <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
+                <h4 className="game-title">Coming Soon</h4>
               </div>
             </div>
           </div>
@@ -100,157 +121,7 @@ export const Games = () => {
                 <Image src={mockImage} alt="Game" width={500} height={500} />
               </div>
               <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="all-games anim-change">
-        <div className="title-cover">
-          <h4 className="games-title">
-            <span className="icon icon-8">
-              <NewReleaseIcon />
-            </span>
-            New Release
-          </h4>
-        </div>
-        <div className="row miscoo-row">
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-3 col-lg-4 col-sm-6">
-            <div className="single-game">
-              <div
-                className={classNames('part-img', styles.gameImageContainer)}
-              >
-                <Image src={mockImage} alt="Game" width={500} height={500} />
-              </div>
-              <div className="part-text">
-                <h4 className="game-title">IceBall 71</h4>
-                <a href="#0" className="def-btn def-small">
-                  Play Now
-                </a>
+                <h4 className="game-title">Coming Soon</h4>
               </div>
             </div>
           </div>
