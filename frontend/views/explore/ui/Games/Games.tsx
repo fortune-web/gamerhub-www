b156e4b7868d06f8656c1646590d0a4eb6409a19
fortune-web <daniel.wong9503@gmail.com>
2022-05-13 19:42:47 +0200

    feat: add cyberpop and piratesland

--- frontend/views/explore/ui/Games/Games.tsx
+++ frontend/views/explore/ui/Games/Games.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
-import mockImage from './assets/img/assets-1.jpg';
-import { AllGenresIcon, NewReleaseIcon } from '../Icons/Icons';
+import { AllGenresIcon } from '../Icons/Icons';
 import styles from './Games.module.scss';
 import classNames from 'classnames';
 import Abeat from './assets/img/Abeats.jpg';
 import MetaMoba from './assets/img/MetaMoba.jpg';
 import Warhands from './assets/img/Warhands.jpg';
+import PiratesLand from './assets/img/PiratesLand.png';
+import CyberPop from './assets/img/CyberPop.png';
 
 export const Games = () => {
   return (
@@ -67,6 +68,54 @@ export const Games = () => {
                   className="def-btn def-small"
                 >
                   Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row miscoo-row">
+          <div className="col-xl-4 col-lg-4 col-sm-6 mb-3">
+            <div className="single-game">
+              <div
+                className={classNames('part-img', styles.gameImageContainer)}
+              >
+                <Image
+                  src={PiratesLand}
+                  alt="Pirates Land"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="part-text">
+                <h4 className="game-title">Pirates Land</h4>
+                <a
+                  href="https://twitter.com/PiratesLandio"
+                  className="def-btn def-small"
+                >
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-sm-6 mb-3">
+            <div className="single-game">
+              <div
+                className={classNames('part-img', styles.gameImageContainer)}
+              >
+                <Image
+                  src={CyberPop}
+                  alt="Cyberpop Metaverse"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="part-text">
+                <h4 className="game-title">Cyberpop Metaverse</h4>
+                <a
+                  href="https://zengeon-cos-assets-1259788321.cos.ap-shanghai.myqcloud.com/PrinceOfPersia/Jenkins/AppFiles/Cyberpop_1.0.0_2022_04_27_23_38_36_V25_Dev.false_Symbols.AWS@USE_NFT_ASSETS.apk"
+                  className="def-btn def-small"
+                >
+                  Play Now
                 </a>
               </div>
             </div>
