80af9cd07932bce4bc8b0cf987272009b5aba273
fortune-web <daniel.wong9503@gmail.com>
2022-07-01 05:22:31 +0900

    add About

--- pages/pre-ido/index.tsx
+++ pages/pre-ido/index.tsx
@@ -7,7 +7,7 @@ import { RootRoutes } from '../../frontend/shared/data/routes';
 import { NewReleases } from '../../frontend/views/explore/container/Games/NewReleases';
 import { Trending } from '../../frontend/views/explore/container/Games/Trending';
 import { MostPopular } from '../../frontend/views/explore/container/Games/MostPopular';
-import About from '../../frontend/views/home/About/About';
+import About from '../../frontend/views/ido/About/About';
 
 export const PreIDOPage: NextPage = () => {
   const { asPath: fullPath } = useRouter();
